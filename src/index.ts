9ef4cce4624c05b0fef416f9f1cd2cf70cfd7827
MrBrown6210 <bite6210@gmail.com>
2021-05-28 17:13:23 +0700

    Add handle app killed handle

--- src/index.ts
+++ src/index.ts
@@ -33,3 +33,31 @@ mongoose
     logger.info('Mongoose connection error')
     logger.error(e)
   })
+
+// CONNECTION EVENTS
+// When successfully connected
+mongoose.connection.on('connected', () => {
+  logger.debug('Mongoose default connection open to ' + dbURI)
+})
+
+// If the connection throws an error
+mongoose.connection.on('error', (err) => {
+  logger.error('Mongoose default connection error: ' + err)
+})
+
+// When the connection is disconnected
+mongoose.connection.on('disconnected', () => {
+  logger.info('Mongoose default connection disconnected')
+})
+
+// If the Node process ends, close the Mongoose connection (ctrl + c)
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    logger.info('Mongoose default connection disconnected through app termination')
+    process.exit(0)
+  })
+})
+
+process.on('uncaughtException', (err) => {
+  logger.error('Uncaught Exception: ' + err)
+})
